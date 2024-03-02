0ca688681527e9b9c030c851c775b31aa5af8602
Samson G. Njogu <samsongikonyo142@gmail.com>
2024-03-01 17:13:28 -0800

    main file

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+ssize_t my_getline(char **lineptr, size_t *size, FILE *file);
+char *my_strtok(char *str, const char *delim);
+void print(void);
+void execute_ls(void);
+
+#endif /* MAIN_H */
